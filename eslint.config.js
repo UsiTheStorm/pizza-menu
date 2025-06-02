09aa170e9fd40b101312ffb31b27eef350292100
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-02 14:40:20 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,28 +5,28 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-config-prettier';
 
 export default [
-    { ignores: ['dist'] },
-    {
-        files: ['**/*.{js,jsx}'],
-        languageOptions: {
-            ecmaVersion: 2020,
-            globals: globals.browser,
-            parserOptions: {
-                ecmaVersion: 'latest',
-                ecmaFeatures: { jsx: true },
-                sourceType: 'module',
-            },
-        },
-        plugins: {
-            'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-            ...reactHooks.configs.recommended.rules,
-            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-            'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-        },
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
     },
-    prettier,
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+  },
+  prettier,
 ];
