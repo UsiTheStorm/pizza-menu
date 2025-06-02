8ed3c6139ec46bf0e373a1a1eb69b650dd835c42
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-02 16:32:23 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Footer() {
-  return <footer>We are currently open</footer>;
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour > openHour && hour < closeHour;
+
+  if (isOpen) {
+    return <footer>We are open until {closeHour}:00</footer>;
+  }
+  return <footer>We are currently closed</footer>;
 }
 
 export default Footer;
