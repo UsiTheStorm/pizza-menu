b983caa1c53c8fb66ad45a5650d7b700ca4294bc
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-08 13:30:43 +0100

    Update order section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
@@ -7,17 +5,30 @@ function Footer() {
 
   const isOpen = hour > openHour && hour < closeHour;
 
-  if (isOpen) {
-    return (
-      <footer className="footer">
-        We are open until {closeHour}:00. Come visit us or order online.
-      </footer>
-    );
-  }
+  // if (isOpen) {
+  //   return (
+  //     <footer className="footer">
+  //       We are open until {closeHour}:00. Come visit us or order online.
+  //     </footer>
+  //   );
+  // }
+  // return (
+  //   <footer className="footer">
+  //     We are currently closed. Come visit us or order online between {openHour}:00 and {closeHour}
+  //     :00
+  //   </footer>
+  // );
   return (
     <footer className="footer">
-      We are currently closed. Come visit us or order online between {openHour}:00 and {closeHour}
-      :00
+      <div className="order">
+        <p>
+          {isOpen
+            ? `We're open until ${closeHour}:00.`
+            : `We are currently closed. We are happy to welcome you between ${openHour}:00 and ${closeHour}:00.`}{' '}
+          Come visit us or order online.
+        </p>
+        <button className="btn">Order</button>
+      </div>
     </footer>
   );
 }
