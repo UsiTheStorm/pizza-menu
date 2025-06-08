bcb381ca0ed0791d0a6cb129f6d4bfe9e36ae8ce
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-06-08 13:02:09 +0100

    Update opening hours sign

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,18 @@ function Footer() {
   const isOpen = hour > openHour && hour < closeHour;
 
   if (isOpen) {
-    return <footer className="footer">We are open until {closeHour}:00</footer>;
+    return (
+      <footer className="footer">
+        We are open until {closeHour}:00. Come visit us or order online.
+      </footer>
+    );
   }
-  return <footer className="footer">We are currently closed</footer>;
+  return (
+    <footer className="footer">
+      We are currently closed. Come visit us or order online between {openHour}:00 and {closeHour}
+      :00
+    </footer>
+  );
 }
 
 export default Footer;
